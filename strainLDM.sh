57fc69954dd84be54c31975ef8ab5ea7ceebe672
Asadur Chowdury <achowdur@umich.edu>
2022-09-27 14:16:22 -0400

    minor changes

--- strainLDM.sh
+++ strainLDM.sh
@@ -3,17 +3,17 @@
 #SBATCH --job-name=ldm
 #SBATCH --mail-type=ALL
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=20
-#SBATCH --mem-per-cpu=4g
-#SBATCH --gres=gpu:4
+#SBATCH --ntasks-per-node=10
+#SBATCH --mem-per-cpu=6g
+#SBATCH --gres=gpu:3
 #SBATCH --time=120:00:00
-#SBATCH --account=precisionhealth_owned1
-#SBATCH --partition=precisionhealth
+#SBATCH --account=precisionhealth_project1
+#SBATCH --partition=gpu
 #SBATCH --mail-user=achowdur@umich.edu
 #SBATCH --export=ALL
 
 # python job to run
 # conda activate ldm
 # python main.py --base models/first_stage_models/vq-f8-n256/config.yaml -r ../../experiment/vqmodel/logs/2022-09-21T01-20-11_config/ -l /nfs/turbo/umms-tocho/code/achowdur/experiment/vqmodel/logs -p bigsrhldm -t True --gpus 0,1,2,3
-python main.py --base configs/latent-diffusion/srh-ldm-vq-8.yaml -l /nfs/turbo/umms-tocho/code/achowdur/experiment/ldm/logs -p uncondldm -t True --gpus 0,1,2,3
+python main.py --base configs/latent-diffusion/srh-ldm-vq-8.yaml -l /nfs/turbo/umms-tocho/code/achowdur/experiment/ldm/logs -p uncondldm -t True --gpus 0,1,2,
 
