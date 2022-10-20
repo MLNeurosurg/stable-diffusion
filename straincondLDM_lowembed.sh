38b32c69f07b7a2dda3f80b11bd3e79c5f2d7da6
Asadur Chowdury <achowdur@umich.edu>
2022-10-20 12:09:24 -0400

    new params

--- straincondLDM_lowembed.sh
+++ straincondLDM_lowembed.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=cldm
+#SBATCH --job-name=cldmv2
 #SBATCH --mail-type=ALL
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=10
@@ -15,5 +15,5 @@
 # python job to run
 # conda activate ldm
 # python main.py --base models/first_stage_models/vq-f8-n256/config.yaml -r ../../experiment/vqmodel/logs/2022-09-21T01-20-11_config/ -l /nfs/turbo/umms-tocho/code/achowdur/experiment/vqmodel/logs -p bigsrhldm -t True --gpus 0,1,2,3
-python main.py --base configs/latent-diffusion/SRH-ldm-vq-f8-condition.yaml -l /nfs/turbo/umms-tocho/code/achowdur/experiment/ldm/logs -p condldm -t True --gpus 0,1,2,
+python main.py --base configs/latent-diffusion/SRH-ldm-vq-f8-condition_emb.yaml -l /nfs/turbo/umms-tocho/code/achowdur/experiment/ldm/logs -p condldmv1 -t True --gpus 0,1,2,
 
