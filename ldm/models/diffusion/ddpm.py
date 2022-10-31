356c0472c6f144a32ab276f83f0a50a615b7e213
achowdur <achowdur@umich.edu>
2022-10-31 15:06:55 -0400

    resize concat vector

--- ldm/models/diffusion/ddpm.py
+++ ldm/models/diffusion/ddpm.py
@@ -1428,6 +1428,7 @@ class DiffusionWrapper(pl.LightningModule):
             cc = torch.cat(c_crossattn, 1)
             out = self.diffusion_model(x, t, context=cc)
         elif self.conditioning_key == 'hybrid':
+            c_concat = [c_concat[0].resize_([8,1,32,32])]
             xc = torch.cat([x] + c_concat, dim=1)
             cc = torch.cat(c_crossattn, 1)
             out = self.diffusion_model(xc, t, context=cc)
